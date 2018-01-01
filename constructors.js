edca8c1dfb93bdb4595bb77c206f22e06a5729bb
Kara Rainey <karain82@hotmail.com>
2017-12-31 19:19:46 -0600

    finished Closures and constructors

--- constructors.js
+++ constructors.js
@@ -29,7 +29,7 @@ function Employee(name, email, hireDate) {
 }
 
 // Code here
-Bob.Employee("Bob", "bob@gmail.com", "01-02-98");
+var bob = new Employee("Bob", "bob@gmail.com", "01-02-98");
 
 
 
@@ -44,7 +44,17 @@ Bob.Employee("Bob", "bob@gmail.com", "01-02-98");
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
 // Code here
+function Car(make, model, year){
+  this.make=make;
+  this.model=model;
+  this.year=year;
+  this.move=0;
+  this.moveCar=function(){
+    this.move += 10;
 
+    return this.move;
+  }
+}
 
 
 
@@ -54,8 +64,16 @@ Bob.Employee("Bob", "bob@gmail.com", "01-02-98");
   var mustang = new Car('Ford', 'Mustang', 2013);
   
   //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+  // var move= 0;
   
-  prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+  //   this.move=function moveCar(make){
+  //     this.make=make;
+
+  //     for(var i=0; i<move.length;i++){
+  //     return move + 10;
+  //   }
+  // }
+  prius.moveCar();   //increments prius' move property by 10. Returns the new move property.
   mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 
